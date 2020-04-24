958515ae1fde93e89a09b8718fb2076989879d9a
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-24 11:04:16 -0700

    tests refactored

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,13 +2,20 @@ const request = require('supertest');
 const server = require('./server');
 const db = require('../database/dbConfig');
 
+/* end points
+
+/api/auth
+/api/jokes
+/api/users
+/
+
+*/
+
 describe('server', () => {
   describe('GET /', () => {
-
     it('should run without errors', () => {
       expect(true).toBe(true);
     })
-
     it('should return 200 OK', () => {
       // make a GET request to / endpoint on the server
       return request(server) // return the async call to let jest know it should wait
@@ -18,7 +25,36 @@ describe('server', () => {
           expect(res.status).toBe(200);
         })
     })
+  })
 
+  describe('POST /api/auth/register', () => {
+    it('should run without errors', () => {
+      expect(true).toBe(true);
+    })
+    it('should return 201 OK', () => {
+      // make a GET request to / endpoint on the server
+      return request(server) // return the async call to let jest know it should wait
+        .get('/')
+        .then( res => {
+          // assert that the HTTP status code is 200
+          expect(res.status).toBe(200);
+        })
+    })
+  })
+
+  describe('POST /api/auth/login', () => {
+    it('should run without errors', () => {
+      expect(true).toBe(true);
+    })
+    it('should return 201 OK', () => {
+      // make a GET request to / endpoint on the server
+      return request(server) // return the async call to let jest know it should wait
+        .get('/')
+        .then( res => {
+          // assert that the HTTP status code is 200
+          expect(res.status).toBe(200);
+        })
+    })
   })
   
 })
