064bf1aaa19828bd0291b63f4f695c9766138aa0
Eddie Madrigal <edmadrigal@yahoo.com>
2020-04-24 10:04:09 -0700

    authentication middleware added

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,29 @@
   before granting access to the next middleware/route handler
 */ 
 
+const bcrypt = require('bcrypt');
+
+const Users = require('../users/users-model');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        }
+        else {
+          res.status(401).json({ message: 'Invalid credentials' })
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Unexpected error!' })
+      })
+  } else {
+    res.status(400).json({ message: 'No credentials provided' })
+  }
+
 };
